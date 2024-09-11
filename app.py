35d314b97b5be8e7649f512362dd56695e2b9279
Sakib_Dalal <sakibdalal73@gmail.com>
2024-09-11 13:03:27 +0530

    Wed Sep 11 13:03:27 IST 2024

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def home():
 def contact():
     return render_template("contact.html")
 
+@app.route('/address')
+def address():
+    return render_template("address.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=80)
